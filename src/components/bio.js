3f2ed537c1956e6beccfc5ae0e04fdd5563cdeb2
ish-xyz <ishamaraia@gmail.com>
2019-09-25 21:24:35 +0100

    Google analytics and bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -58,7 +58,7 @@ const Bio = () => {
         {` `}
         <br></br>
         <a href={`https://twitter.com/${social.twitter}`}>
-          You should really follow me :D
+          You should really follow me on Twitter ^^'
         </a>
       </p>
     </div>
